7ac60ee4deda43afdbb496b97703f096874eb240
campanel <cleber.campanel@gmail.com>
2016-08-07 11:47:59 -0300

    Solicitar autenticação para todas as rotas de  users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,6 +10,9 @@ use App\Http\Requests;
 
 class UserController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
